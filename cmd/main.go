a3f79852b09b2d2eade822aeba37418714238d4e
skotnicky <53302304+skotnicky@users.noreply.github.com>
2025-05-20 13:38:15 +0200

    Fix build with updated controller-runtime options

--- cmd/main.go
+++ cmd/main.go
@@ -1,68 +1,70 @@
 package main
 
 import (
-    "flag"
-    "os"
+	"flag"
+	"os"
 
-    "github.com/skotnicky/openstack-sre-operator/controllers"
-    "k8s.io/apimachinery/pkg/runtime"
-    utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-    clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-    ctrl "sigs.k8s.io/controller-runtime"
-    "sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"github.com/skotnicky/openstack-sre-operator/controllers"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
-    openstackv1alpha1 "github.com/skotnicky/openstack-sre-operator/api/v1alpha1"
+	openstackv1alpha1 "github.com/skotnicky/openstack-sre-operator/api/v1alpha1"
 )
 
 var (
-    scheme   = runtime.NewScheme()
-    setupLog = ctrl.Log.WithName("setup")
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
 )
 
 func init() {
-    utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-    utilruntime.Must(openstackv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(openstackv1alpha1.AddToScheme(scheme))
 }
 
 func main() {
-    var metricsAddr string
-    var enableLeaderElection bool
-    flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-    flag.BoolVar(&enableLeaderElection, "leader-elect", true,
-        "Enable leader election for controller manager. Ensures only one active controller manager.")
+	var metricsAddr string
+	var enableLeaderElection bool
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", true,
+		"Enable leader election for controller manager. Ensures only one active controller manager.")
 
-    opts := zap.Options{
-        Development: false,
-    }
-    opts.BindFlags(flag.CommandLine)
-    flag.Parse()
+	opts := zap.Options{
+		Development: false,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
 
-    ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-    mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-        Scheme:                 scheme,
-        MetricsBindAddress:     metricsAddr,
-        Port:                   9443,
-        LeaderElection:         enableLeaderElection,
-        LeaderElectionID:       "openstack-sre-operator",
-        LeaderElectionNamespace: "default", // or "kube-system"
-    })
-    if err != nil {
-        setupLog.Error(err, "unable to start manager")
-        os.Exit(1)
-    }
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: metricsAddr,
+		},
+		LeaderElection:          enableLeaderElection,
+		LeaderElectionID:        "openstack-sre-operator",
+		LeaderElectionNamespace: "default", // or "kube-system"
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
 
-    if err = (&controllers.OpenStackSREReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "OpenStackSRE")
-        os.Exit(1)
-    }
+	if err = (&controllers.OpenStackSREReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OpenStackSRE")
+		os.Exit(1)
+	}
 
-    setupLog.Info("starting manager")
-    if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-        setupLog.Error(err, "problem running manager")
-        os.Exit(1)
-    }
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
